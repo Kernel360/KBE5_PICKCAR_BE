fcfc830b2e0832d401af813e1be0109fa88b3b88
seomoon <sunghoon1216@naver.com>
2025-05-28 10:37:44 +0900

    feat: Swagger를 통한 API Docs 적용 및 커스텀을 통한 오류 표현
    - DriveHistory를 예시로 Swagger 적용
    - Swagger의 응답 responses부분을 커스텀하여 domain exception도 표현
    - ExampleHolder와 ApiErrorCodeExample 어노테이션 커스텀으로 접근, 사용

--- domain/src/main/java/com/pickcar/drivehistory/presentation/DriveHistoryApiDocs.java
+++ domain/src/main/java/com/pickcar/drivehistory/presentation/DriveHistoryApiDocs.java
@@ -1,6 +1,8 @@
 package com.pickcar.drivehistory.presentation;
 
+import com.pickcar.drivehistory.exception.DriveHistoryErrorCode;
 import com.pickcar.drivehistory.presentation.dto.response.ExampleResponse;
+import com.pickcar.swagger.annotation.ApiErrorCodeExample;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,5 +13,6 @@ public interface DriveHistoryApiDocs {
 
     @Operation(summary = "예시용 API", description = "예시용 API 입니다.")
     @ApiResponse(responseCode = "200", description = "성공했음을 의미합니다")
+    @ApiErrorCodeExample(DriveHistoryErrorCode.class)
     ResponseEntity<ExampleResponse> example();
 }
