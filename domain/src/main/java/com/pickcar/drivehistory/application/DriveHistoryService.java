e3738318e60192134b838960c684b9aee2813e7d
seomoon <sunghoon1216@naver.com>
2025-06-04 14:24:22 +0900

    feat: 운행일지 생성을 위한 기능 기본 틀 작성

--- domain/src/main/java/com/pickcar/drivehistory/application/DriveHistoryService.java
+++ domain/src/main/java/com/pickcar/drivehistory/application/DriveHistoryService.java
@@ -1,33 +1,79 @@
 package com.pickcar.drivehistory.application;
 
+import com.pickcar.application.CycleInfoService;
+import com.pickcar.application.EventInfoService;
+import com.pickcar.auth.application.UserService;
+import com.pickcar.auth.domain.User;
+import com.pickcar.domain.CycleInfo;
+import com.pickcar.domain.EventInfo;
 import com.pickcar.drivehistory.domain.DriveHistory;
-import com.pickcar.drivehistory.exception.DriveHistoryErrorCode;
-import com.pickcar.drivehistory.exception.DriveHistoryException;
 import com.pickcar.drivehistory.infrastructure.DriveHistoryRepository;
-import com.pickcar.drivehistory.presentation.dto.request.DriveHistoryCreateRequest;
+import com.pickcar.reservation.application.ReservationService;
+import com.pickcar.reservation.domain.Reservation;
+import com.pickcar.vehicle.application.VehicleService;
+import com.pickcar.vehicle.domain.Vehicle;
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DriveHistoryService {
 
+    private final UserService userService;
+    private final VehicleService vehicleService;
+    private final ReservationService reservationService;
+    private final CycleInfoService cycleInfoService;
+    private final EventInfoService eventInfoService;
     private final DriveHistoryRepository driveHistoryRepository;
 
     @Transactional
-    public void create(DriveHistoryCreateRequest request) {
-        //TODO: 유효성 검사, request 계산 시기 고려 필요
-        checkCondition(request);
+    public void write(Long reservationId) {
+        Reservation reservation = reservationService.getById(reservationId);
+        User user = userService.getById(reservation.getUserId());
+        Vehicle vehicle = vehicleService.getById(reservation.getVehicleId());
+        Double totalDistance = 0D;
+
+        EventInfo offEventInfo = eventInfoService.getLatestOffEventInfoByVehicleId(vehicle.getId());
+
+        List<CycleInfo> cycleInfoList = cycleInfoService.getCycleInfosByOffEventInfo(offEventInfo);
+
+        for (CycleInfo c : cycleInfoList) {
+            Map<String, Object> cycleInfoMap = c.getCycleInfos();
+
+            log.info("cycleInfoMap : {}", cycleInfoMap.toString());
+
+            for (int i = 0; i < c.getCycleCnt(); i++) {
+
+                String data = cycleInfoMap.get(i).toString();
+
+                String[] split = data.split("totalDistance: ");
+                String[] split2 = split[1].split("battery");
+
+                log.info("detected distance : {}", split2[0]);
+
+                double currentDistance = Double.parseDouble(split2[0]);
+
+                totalDistance += currentDistance;
+            }
+        }
+
         DriveHistory history = DriveHistory.builder()
-                .reservationId(request.reservationId())
-                .drivingStartedAt(request.drivingStartedAt())
-                .drivingEndedAt(request.drivingEndedAt())
-                .totalDrivingTime(request.totalDrivingTime())
-                .totalDistance(request.totalDistance())
+                .drivingStartedAt(offEventInfo.getEngineOnTime())
+                .drivingEndedAt(offEventInfo.getEngineOffTime())
+                .totalDrivingTime(LocalTime.MIDNIGHT.plus(
+                        Duration.between(offEventInfo.getEngineOnTime(), offEventInfo.getEngineOffTime())))
+                .totalDistance(totalDistance)
                 .build();
+
 
         driveHistoryRepository.save(history);
     }
@@ -37,18 +83,18 @@ public class DriveHistoryService {
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] DriveHistory Not Found By Id " + id));
     }
 
-    //FIXME: 메서드 분리 및 네이밍 수정 필요, 구성 순서도 중요
-    public void checkCondition(DriveHistoryCreateRequest request) {
-        if (request.drivingStartedAt().isAfter(LocalDateTime.now())) {
-            throw new DriveHistoryException(DriveHistoryErrorCode.START_TIME_BEFORE_NOW);
-        }
-
-        if (request.drivingEndedAt().isAfter(LocalDateTime.now())) {
-            throw new DriveHistoryException(DriveHistoryErrorCode.END_TIME_BEFORE_NOW);
-        }
-
-        if (request.drivingEndedAt().isBefore(request.drivingStartedAt())) {
-            throw new DriveHistoryException(DriveHistoryErrorCode.END_TIME_BEFORE_START_TIME);
-        }
-    }
+//    //FIXME: 메서드 분리 및 네이밍 수정 필요, 구성 순서도 중요
+//    public void checkCondition(WriteDriveHistoryCommandDto dto) {
+//        if (dto.drivingStartedAt().isAfter(LocalDateTime.now())) {
+//            throw new DriveHistoryException(DriveHistoryErrorCode.START_TIME_BEFORE_NOW);
+//        }
+//
+//        if (dto.drivingEndedAt().isAfter(LocalDateTime.now())) {
+//            throw new DriveHistoryException(DriveHistoryErrorCode.END_TIME_BEFORE_NOW);
+//        }
+//
+//        if (dto.drivingEndedAt().isBefore(dto.drivingStartedAt())) {
+//            throw new DriveHistoryException(DriveHistoryErrorCode.END_TIME_BEFORE_START_TIME);
+//        }
+//    }
 }
--- emulator/src/main/java/com/pickcar/infrastructure/CycleInfoRepository.java
+++ emulator/src/main/java/com/pickcar/infrastructure/CycleInfoRepository.java
@@ -1,7 +1,10 @@
 package com.pickcar.infrastructure;
 
 import com.pickcar.domain.CycleInfo;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CycleInfoRepository extends JpaRepository<CycleInfo, Long> {
+    List<CycleInfo> findByOccurredAtBetween(LocalDateTime occurredAtStart, LocalDateTime occurredAtEnd);
 }
