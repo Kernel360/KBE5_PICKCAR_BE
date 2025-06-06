293a84c45335869586ec3a2650a0353786337b58
seomoon <sunghoon1216@naver.com>
2025-06-06 14:29:18 +0900

    chore: consumer 모듈 사용에 따른 application과 yml 설정
    - consumer application 도입
    - 8082 포트 임시로 사용

---
+++ consumer/src/main/java/com/pickcar/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.pickcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan(basePackages = {
+        "com.pickcar.emulator.domain",
+})
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
