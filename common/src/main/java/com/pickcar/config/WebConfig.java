8bfb7fa7e75fdf645fecd6829b68c0222f8c6d95
seomoon <sunghoon1216@naver.com>
2025-06-06 17:16:20 +0900

    feat: 로컬 테스트용 cors 설정 임시 추가

---
+++ common/src/main/java/com/pickcar/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.pickcar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")        //FIXME: local은 localhost, 배포는 ip 혹은 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
