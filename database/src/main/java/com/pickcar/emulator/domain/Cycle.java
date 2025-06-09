b59fd73783aa90459b612ea33aac9190c745e8b1
seomoon <sunghoon1216@naver.com>
2025-06-09 13:45:31 +0900

    feat: 주기당 이동거리 계산 로직 추가

--- database/src/main/java/com/pickcar/emulator/domain/Cycle.java
+++ database/src/main/java/com/pickcar/emulator/domain/Cycle.java
@@ -15,7 +15,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Entity
 @Getter
 @Builder
@@ -39,4 +41,46 @@ public class Cycle {
     @Convert(converter = CycleInfoConverter.class)
     @Column(columnDefinition = "text")
     private List<CycleInfo> cycleInfos;
+
+    private static final Double EARTH = 6371000.0D;
+
+    public Cycle(Long vehicleId, LocalDateTime occurredAt, Integer cycleCnt, List<CycleInfo> cycleInfos) {
+        this.vehicleId = vehicleId;
+        this.occurredAt = occurredAt;
+        this.cycleCnt = cycleCnt;
+        this.distance = calcCycleDistance(cycleInfos);
+        this.cycleInfos = cycleInfos;
+    }
+
+    private Double calcCycleDistance(List<CycleInfo> cycleInfos) {
+        double totalDistance = 0.0D;
+
+        if (cycleInfos.size() <= 1) {
+            log.info("거리 계산이 필요하지 않은 정보입니다 : {}", cycleInfos);
+        }
+
+        for (int i = 0; i < cycleInfos.size() - 1; i++) {
+            Double originLatitude = cycleInfos.get(i).getLatitude();
+            Double originLongitude = cycleInfos.get(i).getLongitude();
+            Double destLatitude = cycleInfos.get(i + 1).getLatitude();
+            Double destLongitude = cycleInfos.get(i + 1).getLongitude();
+
+            Double lat1Rad = Math.toRadians(originLatitude);
+            Double lat2Rad = Math.toRadians(destLatitude);
+            Double deltaLatitude = Math.toRadians(destLatitude - originLatitude);
+            Double deltaLongitude = Math.toRadians(destLongitude - originLongitude);
+
+            Double a = Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2)
+                    + Math.cos(lat1Rad) * Math.cos(lat2Rad)
+                    * Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2);
+
+            Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+            Double currentDistance = 6371000.0D * c; //지구 반지름 : 약 6371 * 1000(km)
+
+            totalDistance += currentDistance;
+        }
+
+        return totalDistance;
+    }
 }
