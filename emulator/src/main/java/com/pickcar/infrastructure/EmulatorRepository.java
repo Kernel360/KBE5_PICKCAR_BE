4c0d389c133a9026fb041ba19432b78b4a859176
bailey <baileypark@kakao.com>
2025-06-10 23:16:59 +0900

    refactor: 더 이상 사용하지 않는 데이터 삭제

--- emulator/src/main/java/com/pickcar/infrastructure/EmulatorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pickcar.infrastructure;
-
-import com.pickcar.domain.Terminal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmulatorRepository extends JpaRepository<Terminal, Long> {
-
-}
