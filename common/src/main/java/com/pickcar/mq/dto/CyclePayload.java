dd946072a154ddb0425b87ae42ffa3094ebbd0ac
bailey <baileypark@kakao.com>
2025-06-10 23:29:23 +0900

    feat: CyclePayload 생성

---
+++ common/src/main/java/com/pickcar/mq/dto/CyclePayload.java
@@ -0,0 +1,32 @@
+package com.pickcar.mq.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CyclePayload {
+    @JsonProperty("vehicle_id")
+    private Long vehicleId;
+
+    @JsonFormat(pattern = "yyyyMMddHHmmss")
+    @JsonProperty("occurred_at")
+    private LocalDateTime occurredAt;
+
+    @JsonProperty("cycle_cnt")
+    private Integer cycleCnt;
+
+    @JsonProperty("distance")
+    private Integer distance;
+
+    @JsonProperty("cycle_infos")
+    private List<CycleInfoPayload> cycleInfos;
+}
