01b95f63c37c49a98418e815989c14c62c107d38
bailey <baileypark@kakao.com>
2025-06-11 12:26:45 +0900

    refactor: @Repository 삭제

--- consumer/src/main/java/com/pickcar/infrastructure/CycleInfoRepository.java
+++ consumer/src/main/java/com/pickcar/infrastructure/CycleInfoRepository.java
@@ -4,6 +4,5 @@ import com.pickcar.domain.Cycle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CycleInfoRepository extends JpaRepository<Cycle, Long> {
 }
