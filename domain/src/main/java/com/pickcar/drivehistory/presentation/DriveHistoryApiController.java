0bee131641bf4afeb725d3f6ae28ea4ab56a8830
seomoon <sunghoon1216@naver.com>
2025-06-23 18:02:58 +0900

    fix: 운행일지 리스트 필터링시 RequestBody 대신 ModelAttribute 사용
    - RestfulAPI 에서 GET + RequestBody 요청은 옳지 않음

--- domain/src/main/java/com/pickcar/drivehistory/presentation/DriveHistoryApiController.java
+++ domain/src/main/java/com/pickcar/drivehistory/presentation/DriveHistoryApiController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,7 +39,7 @@ public class DriveHistoryApiController implements DriveHistoryApiDocs {
     @Override
     @GetMapping("/list")
     @ResponseStatus(HttpStatus.OK)
-    public List<DriveHistoryListResponse> list(@RequestBody(required = false) DriveHistoryFilterRequest filterRequest) {
+    public List<DriveHistoryListResponse> list(@ModelAttribute DriveHistoryFilterRequest filterRequest) {
         log.info("GET /api/v1/history/list : {} ", filterRequest);
         return driveHistoryService.getFilteredListResponses(filterRequest);
     }
