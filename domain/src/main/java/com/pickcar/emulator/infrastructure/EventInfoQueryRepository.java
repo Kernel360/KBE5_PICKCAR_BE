df65af14e0d9e3a7f113406be57cf0415e4d7438
bailey <baileypark@kakao.com>
2025-07-02 05:20:50 +0900

    feat: findByStatusAndId(boolean status, Long id)를 Optional<EventInfo> findByEventStatusAndId(EventStatus eventStatus, Long id)로 변경

--- domain/src/main/java/com/pickcar/emulator/infrastructure/EventInfoQueryRepository.java
+++ domain/src/main/java/com/pickcar/emulator/infrastructure/EventInfoQueryRepository.java
@@ -1,9 +1,11 @@
 package com.pickcar.emulator.infrastructure;
 
+import com.pickcar.dto.EventStatus;
 import com.pickcar.emulator.domain.EventInfo;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventInfoQueryRepository extends JpaRepository<EventInfo, Long> {
     Optional<EventInfo> findByStatusAndId(boolean status, Long id);
+    Optional<EventInfo> findByEventStatusAndId(EventStatus eventStatus, Long id);
 }
