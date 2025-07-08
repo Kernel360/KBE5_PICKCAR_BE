84dd33ac120efb6bf5724ad9d0e097f32149aeed
SeungGyeong <tpsseung@gmail.com>
2025-07-08 14:00:15 +0900

    feat: token 관련 예외 클래스 및 에러코드 enum 생성
    -파일 생성만 했으며 구체적인 에러 메세지는 추후에 작성 예정

---
+++ domain/src/main/java/com/pickcar/auth/exception/TokenException.java
@@ -0,0 +1,9 @@
+package com.pickcar.auth.exception;
+
+import com.pickcar.exception.GlobalException;
+
+public class TokenException extends GlobalException {
+  public TokenException(TokenErrorCode errorCode) {
+    super(errorCode);
+  }
+}
