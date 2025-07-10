22c3370263a6ead1c02a8c69c37c6b5f5a8bda06
seomoon <sunghoon1216@naver.com>
2025-07-10 18:49:15 +0900

    refactor: 정적 메서드 수집에 맞게 메서드명 변경 및 책임 분리, 매일 자정으로 스케줄링 적용

--- domain/src/main/java/com/pickcar/analytics/application/AnalyticsService.java
+++ domain/src/main/java/com/pickcar/analytics/application/AnalyticsService.java
@@ -8,6 +8,7 @@ import com.pickcar.reservation.application.ReservationService;
 import com.pickcar.reservation.domain.ReservationStatus;
 import com.pickcar.vehicle.application.VehicleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,21 +21,34 @@ public class AnalyticsService {
     private final AnalyticsRepository analyticsRepository;
 
     @Transactional
-    public Analytics getStaticAnalytics() {
+    public void save(StaticInfo staticInfo) {
+        analyticsRepository.save(new Analytics(staticInfo));
+    }
+
+    private StaticInfo collectStaticInfos() {
         //FIXME: 각 서비스를 호출하며 JPA 조회를 하지 않고 JPA 없이 Analytics 레포지토리를 만들어서 context를 한 번에 조회하도록
         Long totalVehicleCount = vehicleService.getAllCount();
         Long reservedVehiclesCount = reservationService.getReservedVehiclesCount();
         Long expectedReturnCount = reservationService.getExpectedReturnCount();
         Long delayedCount = reservationService.getDelayedCount();
 
-        StaticInfo staticInfo = new StaticInfo(
-          totalVehicleCount, reservedVehiclesCount, (totalVehicleCount - reservedVehiclesCount), expectedReturnCount, delayedCount
-        );
+        return new StaticInfo(
+                totalVehicleCount,
+                reservedVehiclesCount,
+                (totalVehicleCount - reservedVehiclesCount),
+                expectedReturnCount,
+                delayedCount);
+    }
 
-        Analytics savedAnalytics = analyticsRepository.save(new Analytics(staticInfo));
+    @Scheduled(cron = "1 0 0 * * *")
+    public void collectInfos() {
+        StaticInfo staticInfo = collectStaticInfos();
+        collectNonStaticInfos();
+        save(staticInfo);
+    }
 
-        //FIXME: 객체 반환 X
-        return savedAnalytics;
+    private void collectNonStaticInfos() {
+        //TODO: 정적 데이터 수집
     }
 
     public StaticAnalyticsResponse getStaticAnalytics(Long vehicleId) {
